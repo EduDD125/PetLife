b6e88c9bc4943d32d874c386c1f6beb62b3bd101
Eduardo Derisso <dudu.derisso@gmail.com>
2024-10-16 22:28:31 -0300

    feat(CareType): createrd enum CareType

---
+++ app/src/main/java/com/example/petlife/domain/model/CareType.kt
@@ -0,0 +1,7 @@
+package com.example.petlife.domain.model
+
+enum class CareType(val description: String) {
+    VETERiNARY("Veterinary care"),
+    PETSHOP("Petshop care"),
+    VACCINATION("Vaccination care");
+}
