8b189cc6b08199924002753187c2c02331a894c9
Eduardo Derisso <dudu.derisso@gmail.com>
2024-10-17 21:48:59 -0300

    feat(UserAreaActivity): creating UserAreaActivity class

---
+++ app/src/main/java/com/example/petlife/ui/userArea/UserAreaActivity.kt
@@ -0,0 +1,36 @@
+package com.example.petlife.ui.userArea
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.ActivityResultLauncher
+import androidx.appcompat.app.AppCompatActivity
+import com.example.petlife.R
+import com.example.petlife.databinding.ActivityMainBinding
+import com.example.petlife.databinding.ActivityUserAreaBinding
+
+class UserAreaActivity : AppCompatActivity() {
+    private val amb: ActivityUserAreaBinding by lazy {
+        ActivityUserAreaBinding.inflate(layoutInflater)
+    }
+
+    private lateinit var petActivityLaucher: ActivityResultLauncher<Intent>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(amb.root)
+
+        setSupportActionBar(amb.customToolbar.customToolbar)
+        supportActionBar?.apply {
+            title = getString(R.string.app_name)
+            subtitle = this@UserAreaActivity.javaClass.canonicalName
+        }
+
+        amb.seeDogsInfo.setOnClickListener {
+            Intent("ACTION_TO_OPEN_DOGS_INFO_SCREEN").apply {
+                petActivityLaucher.launch(this)
+            }
+        }
+
+    }
+}
