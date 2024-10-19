4bb5ed92f4e82fa3df1be0da9e2ac17af0a9cb60
Eduardo Derisso <dudu.derisso@gmail.com>
2024-10-19 18:33:48 -0300

    feat(lastVetVisit, lastPetshopVisit, lastVaccinantion): created activities to edit last visit at petshop or at veterinarian or last vaccination

---
+++ app/src/main/java/com/example/petlife/ui/petArea/lastPestshopDate.kt
@@ -0,0 +1,21 @@
+package com.example.petlife.ui.petArea
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.petlife.R
+
+class lastPestshopDate : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_last_pestshop_date)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
