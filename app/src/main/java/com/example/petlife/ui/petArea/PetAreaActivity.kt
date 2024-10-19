5ac52098fa24ce0c5f5b4f5e17b0d0b9058da79e
Eduardo Derisso <dudu.derisso@gmail.com>
2024-10-19 14:31:37 -0300

    feat(PetAreaActivity, UserAreaActivity): added feature to show pet selected infos in pet area activity

--- app/src/main/java/com/example/petlife/ui/petArea/PetAreaActivity.kt
+++ app/src/main/java/com/example/petlife/ui/petArea/PetAreaActivity.kt
@@ -6,16 +6,33 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.petlife.R
+import com.example.petlife.databinding.ActivityPetAreaBinding
+import com.example.petlife.domain.model.Pet
 
 class PetAreaActivity : AppCompatActivity() {
+    private val paa: ActivityPetAreaBinding by lazy {
+        ActivityPetAreaBinding.inflate(layoutInflater)
+    }
+
+    private var pet: Pet = Pet("", "", "", "", "", "", "", "")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_pet_area)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(paa.root)
+
+        setSupportActionBar(paa.customToolbar.customToolbar)
+        supportActionBar?.apply {
+        }
+
+        intent.getParcelableExtra<Pet>("pet_info")?.also { pet ->
+            paa.petNameTextTv.text = pet.name
+            paa.petSizeTextTv.text = pet.size
+            paa.petColorTextTv.text = pet.color
+            paa.petTypeTextTv.text = pet.type
+            paa.petBirthDateTextTv.text = pet.birthDate
+            paa.petPetshopVisitTextTv.text = pet.lastPetshopVisit
+            paa.petVeterinarianVisitTextTv.text = pet.lastVetVisit
+            paa.petLastVaccinationTextTv.text = pet.lastVaccination
         }
     }
 }
--- app/src/main/java/com/example/petlife/ui/userArea/UserAreaActivity.kt
+++ app/src/main/java/com/example/petlife/ui/userArea/UserAreaActivity.kt
@@ -2,13 +2,12 @@ package com.example.petlife.ui.userArea
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.example.petlife.R
-import com.example.petlife.databinding.ActivityMainBinding
 import com.example.petlife.databinding.ActivityUserAreaBinding
+import com.example.petlife.domain.model.Pet
 
 class UserAreaActivity : AppCompatActivity() {
     private val amb: ActivityUserAreaBinding by lazy {
@@ -17,6 +16,14 @@ class UserAreaActivity : AppCompatActivity() {
 
     private lateinit var petActivityLaucher: ActivityResultLauncher<Intent>
 
+    private var doguinho: Pet = Pet("Toto", "preto", "01/06/2000", "medio",
+                        "cao", "10/10/2024", "12/10/2024",
+                        "15/10/2024")
+
+    private var gatinho: Pet = Pet("Bichano", "branco", "01/06/2000", "pequeno",
+        "gato", "10/10/2024", "12/10/2024",
+        "15/10/2024")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(amb.root)
@@ -24,18 +31,40 @@ class UserAreaActivity : AppCompatActivity() {
         setSupportActionBar(amb.customToolbar.customToolbar)
         supportActionBar?.apply {
             title = getString(R.string.app_name)
-            subtitle = this@UserAreaActivity.javaClass.canonicalName
         }
 
-        petActivityLaucher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+        amb.seeDogsInfoBt.text = doguinho.name
+        amb.seeCattysInfoBt.text = gatinho.name
 
-        }
+        petActivityLaucher = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == RESULT_OK) {
+                result.data?.getParcelableExtra<Pet>("update_pet").let {
+                    if (it?.type == doguinho.type) {
+                        doguinho = it
+                        amb.seeDogsInfoBt.text = doguinho.name
+                    } else if (it?.type == gatinho.type) {
+                        gatinho = it
+                        amb.seeCattysInfoBt.text = it.name
+                    }
+                }
+            }
 
-        amb.seeDogsInfo.setOnClickListener {
+            }
+
+        amb.seeDogsInfoBt.setOnClickListener {
             Intent("ACTION_TO_OPEN_PETS_INFO_SCREEN").apply {
+                putExtra("pet_info", doguinho)
                 petActivityLaucher.launch(this)
             }
         }
 
+        amb.seeCattysInfoBt.setOnClickListener {
+            println(gatinho);
+            Intent("ACTION_TO_OPEN_PETS_INFO_SCREEN").apply {
+                putExtra("pet_info", gatinho)
+                petActivityLaucher.launch(this)
+            }
+        }
     }
 }
