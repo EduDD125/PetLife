9a2516444e1d8a147e507064ba268ef92dfef4ac
Eduardo Derisso <dudu.derisso@gmail.com>
2024-10-19 14:02:05 -0300

    fix(editPetInfos): turned input of type não editable

---
+++ app/src/main/java/com/example/petlife/ui/petArea/editPetInfos.kt
@@ -0,0 +1,21 @@
+package com.example.petlife.ui.petArea
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.petlife.R
+
+class editPetInfos : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_edit_pet_infos)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
